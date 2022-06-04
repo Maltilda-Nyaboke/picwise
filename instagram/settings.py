5e2985ee60fe06cc0cb04216f5c97b472f65c97a
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-06-04 22:41:29 +0300

    style:initialize picwise app

--- instagram/settings.py
+++ instagram/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'picwise.apps.PicwiseConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
