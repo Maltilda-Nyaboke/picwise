8b79119b847dd77c3f9414e03dd6d6dae11a332a
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-06-05 07:45:57 +0300

    feature:saving image function

--- picwise/models.py
+++ picwise/models.py
@@ -2,4 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
-    
+    name = models.CharField(max_length=30)
+    caption = models. TextField()
+    user = models.ForeignKey
+
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()    
