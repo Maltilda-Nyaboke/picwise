e905eb32246ef84d65a7e4669a3d28e99402f5d1
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-06-05 13:39:10 +0300

    feature:check for form validation

--- picwise/views.py
+++ picwise/views.py
@@ -8,5 +8,11 @@ def home(request):
     return render(request,'index.html')
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+    else:    
+        form = UserCreationForm()
     return render(request,'register.html',{'form':form})    
