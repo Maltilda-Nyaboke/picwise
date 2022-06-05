cf485b0aedb63e56194586cc7248a98255c0c715
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-06-06 00:14:44 +0300

    style:register models under admin

--- picwise/admin.py
+++ picwise/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from .models import Image,Comment,Likes,Profile
 
 # Register your models here.
+admin.site.register(Image)
+admin.site.register(Comment)
+admin.site.register(Likes)
+admin.site.register(Profile)
