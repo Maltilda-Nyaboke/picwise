2ddbb93ab450831c7bcd5ac8c2face6fab73936c
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-06-06 13:08:08 +0300

    style:add model importations

--- picwise/models.py
+++ picwise/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django import forms
 import datetime as dt
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save,post_delete
+from django.utils.text import slugify
 
 # from cloudinary.models import CloudinaryField
 
