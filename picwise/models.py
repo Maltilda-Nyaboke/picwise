dc41d6df2d50cbb35e42bfcf008c1228285bbc31
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-06-07 03:19:27 +0300

    style:tags

--- picwise/models.py
+++ picwise/models.py
@@ -6,11 +6,22 @@ import datetime as dt
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save,post_delete
 from django.utils.text import slugify
-
+from django.urls import reverse
 # from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
+def user_directory_path(instance,filename):
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+class Tag (models.Model):
+    title = models.CharField(max_length=255,verbose_name='Tag')
+    slug = models.SlugField(null=False,unique = True)
+
+    class Meta:
+        verbose_name_plural = 'Tags'
+    def get_absolute_url(self):  
+        return reverse('tags', args=(self.slug))  
 
 class Image(models.Model):
     id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
