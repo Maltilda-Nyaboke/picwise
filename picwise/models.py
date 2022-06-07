1ece6a090a37ee7adca7522e9e3e1a8dcd902374
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-06-07 04:57:29 +0300

    bug:migrations

--- picwise/models.py
+++ picwise/models.py
@@ -33,7 +33,7 @@ class Tag (models.Model):
 
 
 class Image(models.Model):
-    uuid = models.UUIDField(primary_key=True,default=uuid.uuid4(),editable=False)
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4(),editable=False)
     image = models.ImageField(upload_to=user_directory_path,verbose_name='image',null=True)
     name = models.CharField(max_length=35)
     caption = models.TextField()
