590eefa78c92f1f44e7d4701e1191a12023903d4
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-06-09 20:57:24 +0300

    bug:deployment

--- instagram/settings.py
+++ instagram/settings.py
@@ -154,10 +154,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 )
 # MEDIA_ROOT = BASE_DIR / 'static/media'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage',
